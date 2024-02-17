cd3dff39a6d56da043423d72405c0d190de52824
kodakr <cryptoknowledge4@gmail.com>
2024-02-17 23:42:04 +0100

    mitigated

---
+++ src/Mitigation/MitigatedWiseLend.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: -- WISE --
+
+pragma solidity =0.8.24;
+import {TransferHelper} from "./TransferHelper.sol";
+ /**
+ @dev This contract represents WiseLending contracts implementation of TransferHelper which itself inherits the broken callOptionalReturn algorithm (doesnt revert on false)
+*/
+contract MitigatedWiseLend is TransferHelper{ 
+
+    function safeTransfer(address Token, address to, uint value) public {
+        _safeTransfer(Token,to,value);
+    }
+
+    function safeTransferFrom(address Token, address from,address to, uint value) public {
+        _safeTransferFrom(Token,from,to,value);
+    }
+
+
+}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -7,17 +7,30 @@ import {TokenReturningFalse} from "../src/Token.sol";
 //==============
 import {OZImplementation} from "../src/OZImplementation.sol";
 import {WiseLendImplementation} from "../src/WiseLendImplementation.sol";
+//=======Mitigation files
+import {MitigatedWiseLend} from "../src/Mitigation/MitigatedWiseLend.sol";
 
 contract CounterTest is Test {
-    OZImplementation openzeppelinImplementation;
-    WiseLendImplementation wiseLendImplementationContract;
-    TokenReturningFalse token;
+    // contract implementing safeTransferFrom with OZ library 
+    OZImplementation openzeppelinImplementation; 
+
+    // contract implementing WiseLend Buggy callOptionalReturn
+    WiseLendImplementation wiseLendImplementationContract; 
+
+    //Meme Token contract returning False only (For testing how OZ and Wiselend callOptionalReturn each handle false return)
+    TokenReturningFalse token; 
+
+    //Mitigated wiselend contract
+    MitigatedWiseLend mitigatedWiselend;
+
+
     address wiseLendUser;
     uint256 public constant MINTED = 100_000;
 
     function setUp() public {
         openzeppelinImplementation = new OZImplementation();
         wiseLendImplementationContract = new WiseLendImplementation();
+        mitigatedWiselend = new MitigatedWiseLend();
         token = new TokenReturningFalse();
         wiseLendUser = makeAddr("wiseLendUser");
 
@@ -32,5 +45,10 @@ contract CounterTest is Test {
     function test_Breaks_On_False_Return_For_Wiselend() public {
         vm.expectRevert();
         wiseLendImplementationContract.safeTransferFrom(address(token), wiseLendUser, address(this), 100);  // Doesnt Revert as Expected
+    }
+
+    function test_Breaks_On_False_Return_for__WiseLend_Mitigated() public {
+        vm.expectRevert();
+        mitigatedWiselend.safeTransferFrom(address(token), wiseLendUser, address(this), 100); // Reverts on false!!!
     }
 }
